d4593219142a37d7eb58f10550b1379aa9782fc3
strangepedia <strangepedia2@gmail.com>
2025-05-04 21:43:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const textElement = document.getElementById("typewriter");
+const phrases = [
+  "قصص غامضة من التاريخ",
+  "ظواهر لا تفسير لها",
+  "أماكن مهجورة وأسرار مخفية",
+  "حقائق غريبة أغرب من الخيال"
+];
+
+let currentPhrase = 0;
+let currentChar = 0;
+let isDeleting = false;
+let speed = 100;
+
+function type() {
+  const current = phrases[currentPhrase];
+  const displayed = current.substring(0, currentChar);
+  textElement.innerHTML = displayed + "<span class='cursor'>|</span>";
+
+  if (!isDeleting && currentChar < current.length) {
+    currentChar++;
+    speed = 100;
+  } else if (isDeleting && currentChar > 0) {
+    currentChar--;
+    speed = 50;
+  } else {
+    if (!isDeleting) {
+      isDeleting = true;
+      speed = 1500;
+    } else {
+      isDeleting = false;
+      currentPhrase = (currentPhrase + 1) % phrases.length;
+      speed = 300;
+    }
+  }
+
+  setTimeout(type, speed);
+}
+
+document.addEventListener("DOMContentLoaded", type);
